ded0acc88f713c0a428ed85de09c1b171fe4f52e
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-18 15:49:37 +0530

    Implement state management for ContactForm using useState hook

--- react_demo/src/components/hooks/useState/ContactForm.jsx
+++ react_demo/src/components/hooks/useState/ContactForm.jsx
@@ -1,53 +1,30 @@
+import { useState } from "react";
 import "./CSS/ContactForm.css";
+
 export const ContactForm = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <div>
         <h1 className="contact-title">Contact Form</h1>
-        <form
-          className="contact-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(
-              `Username: ${username}, Email: ${email}, Message: ${message}`
-            );
-          }}
-        >
+        <form className="contact-form" onSubmit={}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
+            <input type="text" name="username" value={username} onChange={} required />
           </div>
           <div className="form-group">
-            <label htmlFor="email">Password</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" value={email} onChange={} required />
           </div>
           <div className="form-group">
             <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
+            <textarea name="message" value={message} onChange={} required />
           </div>
           <button type="submit">Send Message</button>
         </form>
-
-        <style>{`
-          
-        `}</style>
       </div>
     </>
   );
