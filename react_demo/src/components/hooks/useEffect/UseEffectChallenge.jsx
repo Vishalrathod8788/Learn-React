400004d4c02bcfcf6d6e67b9ac0c9a7f82bb8da0
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-19 16:09:32 +0530

    Add UseEffectChallenge component with initial structure

---
+++ react_demo/src/components/hooks/useEffect/UseEffectChallenge.jsx
@@ -0,0 +1,11 @@
+//? 1. counter that increments when a button is clicked
+//? 2. An input field where users can type their name
+//? 3. The Document Title  will update to show the current count
+
+export const UseEffectChallenge = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
