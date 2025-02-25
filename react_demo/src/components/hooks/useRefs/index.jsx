bb23a4cd3f0f01217e16539ce6adc6680090d343
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-25 17:23:27 +0530

    Implement UseRefs component with form and refs

--- react_demo/src/components/hooks/useRefs/index.jsx
+++ react_demo/src/components/hooks/useRefs/index.jsx
@@ -1,7 +1,28 @@
+import "./index.css";
+import { useRef } from "react";
+
 export const UseRefs = () => {
+  const nameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  console.log(nameRef, passwordRef);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value, passwordRef.current.value);
+  };
+
   return (
-    <>
-      <h1>Hello, UseRefs...</h1>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="Username">Username</label>
+      <br />
+      <input type="text" id="name" ref={nameRef} />
+      <br />
+      <label htmlFor="Password">Password</label>
+      <br />
+      <input type="text" id="password" ref={passwordRef} />
+      <br />
+      <button>Submit</button>
+    </form>
   );
 };
