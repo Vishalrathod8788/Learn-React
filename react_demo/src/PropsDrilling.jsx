69b662df665a1e0c21907359f4ad719cbf2f1c54
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-27 23:19:41 +0530

    Implement props drilling with nested components in PropsDrilling

--- react_demo/src/PropsDrilling.jsx
+++ react_demo/src/PropsDrilling.jsx
@@ -4,6 +4,24 @@ export const ParentComponent = () => {
       className={`p-4 h-lvh w-lvw font-sans font-display tracking-wider flex flex-col items-center justify-center bg-gray-900 text-white`}
     >
       <h1>Component A</h1>
+      <ChildComponent data="React JS" />
+    </section>
+  );
+};
+const ChildComponent = ({ data }) => {
+  return (
+    <section className="p-4 h-lvh w-lvw font-sans font-display tracking-wider flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1>Component B</h1>
+      <GrandChildComponent data={data} />
+    </section>
+  );
+};
+
+const GrandChildComponent = ({ data }) => {
+  return (
+    <section className="p-4 h-lvh w-lvw font-sans font-display tracking-wider flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1>Component C</h1>
+      <p>Hello, I Love {data}</p>
     </section>
   );
 };
