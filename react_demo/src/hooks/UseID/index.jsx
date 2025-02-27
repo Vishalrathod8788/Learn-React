d71eafe5c66802b19dfdcf381202df7cdee7f0c7
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-27 22:53:06 +0530

    Add password input and improve useId hook usage in form

--- react_demo/src/hooks/UseID/index.jsx
+++ react_demo/src/hooks/UseID/index.jsx
@@ -1,18 +1,21 @@
 import { useId } from "react";
 
 export const UseID = () => {
-  const usernameId = useId();
-  const emailId = useId();
+  const id = useId();
 
   return (
     <form>
       <div>
-        <label htmlFor={usernameId}>Username</label>
-        <input type="text" id={usernameId} name="username" />
+        <label htmlFor={id + "usernameId"}>Username</label>
+        <input type="text" id={id + "usernameId"} name="username" />
       </div>
       <div>
-        <label htmlFor={emailId}>Email</label>
-        <input type="email" id={emailId} name="email" />
+        <label htmlFor={id + "passwordId"}>Password</label>
+        <input type="password" id={id + "passwordId"} name="passwordId" />
+      </div>
+      <div>
+        <label htmlFor={id + "emailId"}>Email</label>
+        <input type="email" id={id + "emailId"} name="email" />
       </div>
       <button type="submit">Submit</button>
     </form>
