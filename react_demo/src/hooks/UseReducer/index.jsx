390cce5759b388145f6b1e20c86e3f8e37b53cf3
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-04 17:38:56 +0530

    Refactor UseReducer hook for improved readability and state handling

--- react_demo/src/hooks/UseReducer/index.jsx
+++ react_demo/src/hooks/UseReducer/index.jsx
@@ -1,12 +1,11 @@
 import { useReducer } from "react";
 
 export const UseReducer = () => {
-  
   const initialState = {
     count: 0,
   };
 
-  const reducre = (state, action) => {
+  const reducer = (state, action) => {
     switch (action.type) {
       case "INCREMENT":
         return { count: state.count + 1 };
@@ -19,17 +18,17 @@ export const UseReducer = () => {
     }
   };
 
-  const [count, dispatch] = useReducer(reducre, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleIncre = () => {
+  const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
   };
 
-  const handleDecre = () => {
-    dispatch({ type: "DECREMENT" });
-    if (count === 0) {
+  const handleDecrement = () => {
+    if (state.count === 0) {
       return;
     }
+    dispatch({ type: "DECREMENT" });
   };
 
   return (
@@ -37,20 +36,20 @@ export const UseReducer = () => {
       <div className="p-4 h-lvh flex flex-col justify-center items-center">
         <h1 className="text-3xl font-bold">{state.count}</h1>
         <button
-          className=" bg-slate-400 p-2 rounded-md m-2 "
-          onClick={handleIncre}
+          className="bg-slate-400 p-2 rounded-md m-2"
+          onClick={handleIncrement}
         >
           Increment
         </button>
         <button
-          className=" bg-slate-400 p-2 rounded-md m-2 "
-          onClick={handleDecre}
+          className="bg-slate-400 p-2 rounded-md m-2"
+          onClick={handleDecrement}
         >
           Decrement
         </button>
         <button
           onClick={() => dispatch({ type: "RESET" })}
-          className=" bg-slate-400 p-2 rounded-md m-2 "
+          className="bg-slate-400 p-2 rounded-md m-2"
         >
           Reset
         </button>
