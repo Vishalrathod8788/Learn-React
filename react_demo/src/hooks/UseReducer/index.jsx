8a50f85fab1111df3718dd1fa8e56d1b49e4d83f
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-04 16:54:41 +0530

    Add RESET action to UseReducer hook for state management

--- react_demo/src/hooks/UseReducer/index.jsx
+++ react_demo/src/hooks/UseReducer/index.jsx
@@ -3,11 +3,14 @@ import { useReducer } from "react";
 export const UseReducer = () => {
   const reducre = (state, action) => {
     if (action.type === "INCREMENT") {
+      console.log(state);
       if (state === 20) return 20;
       return state + 1;
     } else if (action.type === "DECREMENT") {
-        if (state === 0) return 0;
+      if (state === 0) return 0;
       return state - 1;
+    } else if (action.type === "RESET") {
+      return 0;
     }
   };
   const [count, dispatch] = useReducer(reducre, 0);
@@ -38,6 +41,12 @@ export const UseReducer = () => {
           onClick={handleDecre}
         >
           Decrement
+        </button>
+        <button
+          onClick={() => dispatch({ type: "RESET" })}
+          className=" bg-slate-400 p-2 rounded-md m-2 "
+        >
+          Reset
         </button>
       </div>
     </>
