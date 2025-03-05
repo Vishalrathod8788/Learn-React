4a5582955b0a1220b538e87f3c5433217e6bcf44
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-03-05 22:53:17 +0530

    Add MemoCount component to ReactMemo for enhanced UI

--- react_demo/src/hooks/Memo/MemoCount.jsx
+++ react_demo/src/hooks/Memo/MemoCount.jsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
-
-export const MemoCount = () => {
-  
+import React, { useRef } from "react";
+const Counts = () => {
+  const renderCount = useRef(0);
+  return (
+    <>
+      <div className="mt-3 font-display text-center">
+        <p className="">Nothing Change here but I've now rendered:</p>
+        <span className="text-red-600">{renderCount.current++}</span>
+      </div>
+    </>
+  );
 };
+
+export default React.memo(Counts);
